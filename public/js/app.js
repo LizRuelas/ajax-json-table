0d4acf06fb2020720be2a686216310a6ae646c6b
LizRuelas <liz.ruelas.borda@gmail.com>
2016-11-04 16:23:17 -0500

    iniciando repo

---
+++ public/js/app.js
@@ -0,0 +1,28 @@
+$(document).ready(function(){
+	$.ajax({
+		url : "http://localhost:3002/demo.json",
+		type : "GET",
+		success : function(response){
+			var nombre = "<ul>";
+			for (var i = 0  ; i<response.length; i++) {
+				nombre += "<li>" + response[i].nombre+"</li>";
+			}
+			nombre += "</ul>";
+			$("#nombres").html(nombre);
+
+			var correo = "<ul>";
+			for (var i = 0  ; i<response.length; i++) {
+				correo += "<li>" + response[i].correo+"</li>";
+			}
+			correo += "</ul>";
+			$("#correos").html(correo);
+
+			var edad = "<ul>";
+			for (var i = 0  ; i<response.length; i++) {
+				edad += "<li>" + response[i].edad+"</li>";
+			}
+			correo += "</ul>";
+			$("#edades").html(edad);
+		}
+	});
+});
